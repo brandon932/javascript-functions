f121c52a54b014cd4e21e62c16e078a2e6ddf2be
brandon932 <bthornton_932@yahoo.com>
2015-07-13 23:14:47 -0600

    added oddNumber fuction

--- main.js
+++ main.js
@@ -51,3 +51,21 @@ function isDivisible(i,n){
   }
 }
 isDivisible(8,3);
+
+function oddNumbers(){
+  var randomNum = Math.random()*101;
+if (randomNum<= 40){
+  for (var i = 0; i < 40; i++ ){
+      if (i % 2 !== 0){
+        console.log(i);
+      }
+    }
+  } else {
+    for (var n = 40; n<= randomNum;n++){
+      if (n % 2 !== 0){
+        console.log(n);
+      }
+    }
+  }
+}
+oddNumbers();
